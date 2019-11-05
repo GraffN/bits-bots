d28c2cc4e9ca7ca45e52385816c101c0147a2ec4
GraffN <jorgen.graff-n@outlook.com>
2019-11-05 15:42:48 +0100

    Updates

---
+++ precache-manifest.d3b68f1d32fc78a0295cdd046a84dc64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758c3386984c6e5143a737dbb8ae0ea0",
+    "url": "/bits-bots/index.html"
+  },
+  {
+    "revision": "ab650926a352cf87325c",
+    "url": "/bits-bots/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a915d8a782e473043519",
+    "url": "/bits-bots/static/js/2.f8aa9d3c.chunk.js"
+  },
+  {
+    "revision": "ab650926a352cf87325c",
+    "url": "/bits-bots/static/js/main.55988777.chunk.js"
+  },
+  {
+    "revision": "c5d89b0a4cae40b06d4c",
+    "url": "/bits-bots/static/js/runtime-main.5bf804bf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bits-bots/static/media/logo.25bf045c.svg"
+  }
+]);
